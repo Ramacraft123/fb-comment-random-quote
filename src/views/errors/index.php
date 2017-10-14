337169c9ac8775d8990ef97ed3260ba469da9fcc
Ricky Andhi <ricky10andika@gmail.com>
2017-10-14 23:01:48 +0700

    Show error in error page

--- src/views/errors/index.php
+++ src/views/errors/index.php
@@ -4,7 +4,7 @@
 	<meta charset="UTF-8">
 	<meta property="og:type" content="article" />
 	<meta property="og:title" content="Awas, chuck norris udah nemuin kamu" />
-	<meta property="og:description" content="Awas ada chuck norris" />
+	<meta property="og:description" content="<?= $errorMessage ?>" />
 	<meta property="og:image" content="http://vignette2.wikia.nocookie.net/uncyclopedia/images/d/dc/Chuck-norris-002.jpg/revision/latest?cb=20080312162141" />
 	<title>Awas, chuck norris udah nemuin kamu</title>
 </head>
