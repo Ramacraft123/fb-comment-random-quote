b656be8e6eebecafb5f68a110a4a1d9f27316c6f
Ricky Andhi <ricky10andika@gmail.com>
2017-10-14 23:01:59 +0700

    Fix title in comment share

--- src/views/random-fb.php
+++ src/views/random-fb.php
@@ -2,8 +2,8 @@
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
-	<meta property="og:type" content="article" />
-	<meta property="og:title" content="<?= $quote['title'] ?>" />
+	<meta property="og:type" content="books.quotes" />
+	<meta property="og:title" content="<?= $quoteContent ?>" />
 	<meta property="og:description" content="<?= $quoteContent ?>" />
 	<meta property="og:image" content="<?= $quote['picture_url'] ?>" />
 	<title><?= $quote['title'] ?></title>
