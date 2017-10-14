d06101077dcc6b7ca98182c95b7665c2715b71c7
Ricky Andhi <ricky10andika@gmail.com>
2017-10-14 16:54:01 +0700

    Fix false quote content

--- src/views/random-fb.php
+++ src/views/random-fb.php
@@ -4,11 +4,10 @@
 	<meta charset="UTF-8">
 	<meta property="og:type" content="article" />
 	<meta property="og:title" content="<?= $quote['title'] ?>" />
-	<meta property="og:description" content="<?= $quote['content'] ?>" />
+	<meta property="og:description" content="<?= $quoteContent ?>" />
 	<meta property="og:image" content="<?= $quote['picture_url'] ?>" />
 	<title><?= $quote['title'] ?></title>
 </head>
 <body>
-	<h1><?= $quoteContent ?></h1>
 </body>
 </html>
